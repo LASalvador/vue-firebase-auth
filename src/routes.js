77b787679f1db543ca7a842c1325013630f49c35
Lucas Salvador <lucas@superclient.com.br>
2019-08-02 14:04:24 -0300

    atualizando rotas

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,30 @@ import Home from './components/Home'
 import SignIn from './components/SignIn'
 
 export default [
-    { path:'/login' , component: Login},
-    { path:'/' , component: Home},
-    { path:'/registrar' , component: SignIn },
+    { 
+        path:'*', 
+        redirect: '/login'
+    },
+    { 
+        path:'/' , 
+        redirect: '/login' 
+    },
+    { 
+        path:'/login' ,
+        name: 'login',
+        component: Login
+    },
+    { 
+        path:'/registrar' , 
+        name: 'registrar',
+        component: SignIn 
+    },
+    { 
+        path:'/home' , 
+        name: 'home',
+        component: Home,
+        meta: {
+            requiresAuth: true
+        }
+    },
 ]
